9bf8e93892507cf7bd04cd768422d989edcce53d
abhayp2204 <abhaypatil1020@gmail.com>
2022-09-09 16:52:22 +0530

    router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import './App.css'
 import Navbar from "./Navbar"
 import axios from "axios"
+// Routes
+import { BrowserRouter as Router, Routes, Route, Outlet } from "react-router-dom"
 
 function App() {
     return (
         <div className="App">
             <Navbar />
             <Router>
-                
+
             </Router>
         </div>
     );
