361539c9cc32e80dcff0f0ad565085f06937a9d4
abhayp2204 <abhaypatil1020@gmail.com>
2022-09-09 16:55:12 +0530

    routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 import './App.css'
+
 import Navbar from "./Navbar"
+import Device from "./Device";
+import Select from "./Select";
+import Navbar from "./Navbar";
+import About from "./About";
+
 import axios from "axios"
 // Routes
 import { BrowserRouter as Router, Routes, Route, Outlet } from "react-router-dom"
@@ -9,7 +15,24 @@ function App() {
         <div className="App">
             <Navbar />
             <Router>
-
+                <Routes>
+                    <Route
+                        path="/about"
+                        element={<About />}
+                    />
+                    <Route
+                        path="/"
+                        element={<Select/>}
+                    />
+                    <Route
+                        path="/kettle"
+                        element={<Device field={1} name="Kettle" />}
+                    />
+                    <Route
+                        path="/hairdryer"
+                        element={<Device field={2} name="Hair Dryer" />}
+                    />
+                </Routes>
             </Router>
         </div>
     );
